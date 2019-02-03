f40df14df75461f90a7757acf9c446dbc3c2cdf0
Erik Sampayo <ErikSampayo@gmail.com>
2019-02-02 19:08:57 -0500

    create controllable character

---
+++ nothing_BASIC/Assets/Yurowm/Demo/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+public class PlayerController : MonoBehaviour {
+
+	public Transform rightGunBone;
+	public Transform leftGunBone;
+	public Arsenal[] arsenal;
+    public Actions actions;
+    public float turningSpeed;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Awake() {
+		animator = GetComponent<Animator> ();
+		if (arsenal.Length > 0)
+			SetArsenal (arsenal[0].name);
+		}
+
+	public void SetArsenal(string name) {
+		foreach (Arsenal hand in arsenal) {
+			if (hand.name == name) {
+				if (rightGunBone.childCount > 0)
+					Destroy(rightGunBone.GetChild(0).gameObject);
+				if (leftGunBone.childCount > 0)
+					Destroy(leftGunBone.GetChild(0).gameObject);
+				if (hand.rightGun != null) {
+					GameObject newRightGun = (GameObject) Instantiate(hand.rightGun);
+					newRightGun.transform.parent = rightGunBone;
+					newRightGun.transform.localPosition = Vector3.zero;
+					newRightGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+					}
+				if (hand.leftGun != null) {
+					GameObject newLeftGun = (GameObject) Instantiate(hand.leftGun);
+					newLeftGun.transform.parent = leftGunBone;
+					newLeftGun.transform.localPosition = Vector3.zero;
+					newLeftGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+				}
+				animator.runtimeAnimatorController = hand.controller;
+				return;
+				}
+		}
+	}
+
+    public void Update()
+    {
+        animator.SetFloat("Speed", Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime);
+    }
+
+
+    [System.Serializable]
+	public struct Arsenal {
+		public string name;
+		public GameObject rightGun;
+		public GameObject leftGun;
+		public RuntimeAnimatorController controller;
+	}
+}
