68c65aa874fa3f5d04440319d5a856fa5eb54eaf
Brian Kalish <bkalish42@gmail.com>
2019-02-04 02:05:16 -0500

    Skeleton Trap Added
    It spawns a skeleton that will follow you. It isn't dangerous yet, but that can be fixed later.

---
+++ nothing_BASIC/Assets/SimpleSkeletonFree/Script/Character/SkeletonMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SkeletonEditor
+{
+
+    public class SkeletonMove : MonoBehaviour
+    {
+        GameObject player;
+        NavMeshAgent nav;   
+
+
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            nav.SetDestination(player.transform.position);
+        }
+    }
+}
---
+++ nothing_BASIC/Assets/Yurowm/Demo/Scripts/SkeletonTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonTrap : MonoBehaviour
+{
+    public GameObject creation;
+    public Vector3 spawnLoc;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Instantiate(creation, spawnLoc, new Quaternion(0, 0, 0, 0));
+    }
+}
