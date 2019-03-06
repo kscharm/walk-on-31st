5de65438bc6c0637dc9b4f809b3fa4c82df64069
Hudock, Samantha Marie <shudock3@gatech.edu>
2019-03-06 17:01:50 -0500

    UI Scripts added

---
+++ nothing_BASIC/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private CanvasGroup canvas;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        canvas = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp (KeyCode.Escape)) {
+            if (canvas.interactable)
+            {
+                canvas.interactable = false;
+                canvas.blocksRaycasts = false;
+                canvas.alpha = 0f;
+                Time.timeScale = 1f;
+            } else
+            {
+                canvas.interactable = true;
+                canvas.blocksRaycasts = true;
+                canvas.alpha = 1f;
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
---
+++ nothing_BASIC/Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneName;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
