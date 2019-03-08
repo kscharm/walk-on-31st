473c167515743f1cd71682421647e67cd362dd45
Ryan <jpower30@gatech.edu>
2019-03-07 21:50:09 -0500

    Cleaned up PlayerMovement

--- nothing_BASIC/Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
+++ nothing_BASIC/Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
@@ -6,23 +6,18 @@ using UnityEngine.EventSystems;
 public class PlayerMovement : MonoBehaviour
 {
     public Animator animator;
-    //public NavMeshAgent agent;
     public SaveData playerSaveData;
-    public float turnSmoothing = 15f;
     public float speedDampTime = 0.1f;
-    public float slowingSpeed = 0.175f;
-    public float turnSpeedThreshold = 0.5f;
+
     public float inputHoldDelay = 0.5f;
     public float playerSpeed; //is dynamically altered based on movement keys
     public float rotateSpeed; 
 
-    private double maxInteractionDistance = 1.5;
-    private float ikWeight = 0;
+    // private double maxInteractionDistance = 1.5;
+    // private float ikWeight = 0;
 
-    Vector3 direction;
-    Quaternion rotatePlayer;
     private Rigidbody rbody;
-    bool holdingSkey = false;
+
     
 
     private Interactable currentInteractable;
@@ -38,16 +33,13 @@ public class PlayerMovement : MonoBehaviour
     public const string startingPositionKey = "starting position";
 
 
-    private const float stopDistanceProportion = 0.1f;
-    private const float navMeshSampleDistance = 4f;
+    // private const float stopDistanceProportion = 0.1f;
 
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
-
-        //agent.updateRotation = false;
 
         inputHoldWait = new WaitForSeconds (inputHoldDelay);
 
@@ -64,9 +56,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnAnimatorMove()
     {
-        //agent.velocity = animator.deltaPosition / Time.deltaTime;
-        
-        //Set player's velocity here
+        //Still necessary despite being empty
     }
 
 
@@ -79,81 +69,7 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat(hashSpeedPara, speed, speedDampTime, Time.deltaTime);
 
-        /*
-        if (agent.pathPending)
-            return;
-        if (agent.remainingDistance <= agent.stoppingDistance * stopDistanceProportion)
-            Stopping (out speed);
-        else if (agent.remainingDistance <= agent.stoppingDistance)
-            Slowing(out speed, agent.remainingDistance);
-        else if (speed > turnSpeedThreshold)
-            Moving ();
-        */
     }
-
-    /*
-
-    private void Stopping (out float speed)
-    {
-        agent.Stop();
-
-        transform.position = destinationPosition;
-        speed = 0f;
-
-        if (currentInteractable)
-        {
-            transform.rotation = currentInteractable.interactionLocation.rotation;
-            currentInteractable.Interact();
-            currentInteractable = null;
-
-            StartCoroutine (WaitForInteraction ());
-        }
-    }
-
-
-    private void Slowing (out float speed, float distanceToDestination)
-    {
-        agent.Stop();
-
-        float proportionalDistance = 1f - distanceToDestination / agent.stoppingDistance;
-
-        Quaternion targetRotation = currentInteractable ? currentInteractable.interactionLocation.rotation : transform.rotation;
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, proportionalDistance);
-
-        transform.position = Vector3.MoveTowards(transform.position, destinationPosition, slowingSpeed * Time.deltaTime);
-
-        speed = Mathf.Lerp(slowingSpeed, 0f, proportionalDistance);
-    }
-
-
-    private void Moving ()
-    {
-        Quaternion targetRotation = Quaternion.LookRotation(agent.desiredVelocity);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSmoothing * Time.deltaTime);
-    }
-
-    */
-
-
-    // public void OnGroundClick(BaseEventData data)
-    // {
-    //     if(!handleInput)
-    //         return;
-        
-    //     currentInteractable = null;
-
-    //     PointerEventData pData = (PointerEventData)data;
-
-    //     NavMeshHit hit;
-    //     if (NavMesh.SamplePosition (pData.pointerCurrentRaycast.worldPosition, out hit, navMeshSampleDistance, NavMesh.AllAreas))
-    //         destinationPosition = hit.position;
-    //     else
-    //         destinationPosition = pData.pointerCurrentRaycast.worldPosition;
-
-    //     agent.SetDestination(destinationPosition);
-    //     agent.Resume ();
-    // }
-
 
     public void OnInteractableClick(Interactable interactable)
     {
@@ -172,11 +88,6 @@ public class PlayerMovement : MonoBehaviour
 
             StartCoroutine(WaitForInteraction());
         }
-
-        /*
-        agent.SetDestination(destinationPosition);
-        agent.Resume();
-        */
 
 
         //Needs to only do this if the interactable is collectable
@@ -260,11 +171,4 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    /*
-         private void rotateTowards(Transform target) {
-            Vector3 direction = (target.position - agent.transform.position).normalized;
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(agent.transform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
-     }
-     */
 }
