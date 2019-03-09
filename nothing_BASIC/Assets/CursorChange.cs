f24cc80737e5237ff867405897dea49bd5913298
shudock3 <Sammi Hudock>
2019-03-08 20:50:07 -0500

    sounds and cursor in place

---
+++ nothing_BASIC/Assets/CursorChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour
+{
+    public Texture2D cursor;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
