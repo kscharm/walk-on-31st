520941664950414d2989c4fe371bb6e47e542116
Ryan <jpower30@gatech.edu>
2019-03-09 21:44:38 -0500

    Door + Key to go from Lvl0 to Lvl2

---
+++ nothing_BASIC/Assets/ChangeScenes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    private bool doorOpened = false;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.GetBool("move")) {
+            SceneManager.LoadScene("LevelTwo");
+        }
+    }
+}
