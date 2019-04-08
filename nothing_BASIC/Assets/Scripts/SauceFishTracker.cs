40186e15a8aeac82de4168e40d5fd0641123bf02
Ryan <jpower30@gatech.edu>
2019-04-07 22:25:52 -0400

    Clicking on type of cauldron adds sprite to inventory, LevelOne starting position, remove duplicates from Inventory for now

---
+++ nothing_BASIC/Assets/Scripts/SauceFishTracker.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauceFishTracker : MonoBehaviour
+{
+    private bool spicySelected = false;
+    private bool sourSelected = false;
+    private bool sweetSelected = false;
+    private bool saltySelected = false;
+    private bool creamySelected = false;
+    private bool sardinesSelected = false;
+    private bool tunaSelected = false;
+    private bool catfishSelected = false;
+    private bool salmonSelected = false;
+    private bool codSelected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Changes boolean values when cauldrons are selected. 
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.name == "SpicyInteractable") {
+                    spicySelected = true;
+                    sourSelected = false;
+                    sweetSelected = false;
+                    saltySelected = false;
+                    creamySelected = false;
+                } else if (hit.transform.name == "SourInteractable") {
+                    spicySelected = false;
+                    sourSelected = true;
+                    sweetSelected = false;
+                    saltySelected = false;
+                    creamySelected = false;
+                } else if (hit.transform.name == "SweetInteractable") {
+                    spicySelected = false;
+                    sourSelected = false;
+                    sweetSelected = true;
+                    saltySelected = false;
+                    creamySelected = false;
+                } else if (hit.transform.name == "SaltyInteractable") {
+                    spicySelected = false;
+                    sourSelected = false;
+                    sweetSelected = false;
+                    saltySelected = true;
+                    creamySelected = false;
+                } else if (hit.transform.name == "CreamyInteractable") {
+                    spicySelected = false;
+                    sourSelected = false;
+                    sweetSelected = false;
+                    saltySelected = false;
+                    creamySelected = true;
+                } else if (hit.transform.name == "SardinesInteractable") {
+                    sardinesSelected = true;
+                    tunaSelected = false;
+                    catfishSelected = false;
+                    salmonSelected = false;
+                    codSelected = false;
+                } else if (hit.transform.name == "TunaInteractable") {
+                    sardinesSelected = false;
+                    tunaSelected = true;
+                    catfishSelected = false;
+                    salmonSelected = false;
+                    codSelected = false;
+                } else if (hit.transform.name == "CatfishInteractable") {
+                    sardinesSelected = false;
+                    tunaSelected = false;
+                    catfishSelected = true;
+                    salmonSelected = false;
+                    codSelected = false;
+                } else if (hit.transform.name == "SalmonInteractable") {
+                    sardinesSelected = false;
+                    tunaSelected = false;
+                    catfishSelected = false;
+                    salmonSelected = true;
+                    codSelected = false;
+                } else if (hit.transform.name == "CodInteractable") {
+                    sardinesSelected = false;
+                    tunaSelected = false;
+                    catfishSelected = false;
+                    salmonSelected = false;
+                    codSelected = true;
+                }
+            }
+        }
+    }
+
+    public bool isCorrectCombination() {
+        //Check if certain booleans are true
+        return false;
+    }
+}
