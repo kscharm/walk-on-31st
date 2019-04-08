081406ab312ed8d110e2928a18b78c841d86ee93
Gouri, Nandan J <ngouri3@gatech.edu>
2019-04-08 01:51:47 -0400

    Revert "Revert "Mixing sauce/fish and feeding to goblin logic works!""
    This reverts commit 76f0766c73e4db3bc0c1c62026df94ede8a9c8f1.

---
+++ nothing_BASIC/Assets/Scripts/GoblinConditionals.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinConditionals : MonoBehaviour
+{
+
+    public Condition correctCombo1;
+    //public SauceFishTracker tracker;
+    public GameObject mixingPot;
+    private SauceFishTracker tracker;
+
+    void Start() {
+        tracker = mixingPot.GetComponent<SauceFishTracker>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Spicy + Sardines
+        if (tracker.isCorrectCombination()) {
+            correctCombo1.satisfied = true;
+        } else {
+            correctCombo1.satisfied = false;
+        }
+    }
+}
