762aaed27f31d4664301bd1c677d34d8add2596b
Kenny Scharm <kscharm3@gatech.edu>
2019-04-11 19:43:54 -0400

    Added electricity to fence and changed generator asset

---
+++ nothing_BASIC/Assets/Scripts/ExitController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            
+        }
+    }
+}
