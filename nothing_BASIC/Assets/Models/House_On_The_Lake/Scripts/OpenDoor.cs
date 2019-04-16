a55d5067abf7802ecb01e9f02cc5c75e9b9d108d
Kenny Scharm <kscharm3@gatech.edu>
2019-04-15 22:59:19 -0400

    Fixed level 0 doors and interactions, added ladle to level 1, and fixed level 1 doors and interactions

---
+++ nothing_BASIC/Assets/Models/House_On_The_Lake/Scripts/OpenDoor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	public float smooth = 2.0f;
+	public float DoorOpenAngle = 90.0f;
+
+	public AudioClip OpenAudio;
+	public AudioClip CloseAudio;
+    public Condition pickedUpKey;
+	private bool AudioS;
+
+	private Vector3 defaultRot;
+	private Vector3 openRot;
+	private bool open;
+	private bool enter;
+
+	// Use this for initialization
+	void Start () {
+		
+			defaultRot = transform.eulerAngles;
+			openRot = new Vector3 (defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open) {
+			if (AudioS == false) {
+				gameObject.GetComponent<AudioSource> ().PlayOneShot (OpenAudio);
+				AudioS = true;
+			}
+			transform.eulerAngles = Vector3.Slerp (transform.eulerAngles, openRot, Time.deltaTime * smooth);
+		} else {
+			if (AudioS == true) {
+				gameObject.GetComponent<AudioSource> ().PlayOneShot (CloseAudio);
+				AudioS = false;
+			}
+			transform.eulerAngles = Vector3.Slerp (transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+
+		}
+		if (Input.GetKeyDown (KeyCode.F) && enter) {
+			open = !open;
+		}
+    }
+
+    private void openDoor()
+    {
+        if (pickedUpKey.satisfied)
+        {
+            open = true;
+        }
+    }
+}
---
+++ nothing_BASIC/Assets/Models/House_On_The_Lake/Shaders/MirrorReflection.shader
@@ -0,0 +1,55 @@
+﻿
+Shader "FX/Mirror Reflection" {
+	Properties{
+		_Color("Main Color", Color) = (1,1,1,1)
+		_MainTex("Base (RGB)", 2D) = "white" {}
+	_BumpMap("Normalmap", 2D) = "bump" {}
+
+	_ReflAmount("Reflection amount", float) = 0.5
+		_ReflDistort("Reflection distort", float) = 0.25
+		_ReflectionTex("Reflection", 2D) = "white" { TexGen ObjectLinear }
+	}
+
+		SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		CGPROGRAM
+#pragma surface surf Lambert
+		//#pragma surface surf BlinnPhong
+
+		struct Input {
+		float2 uv_MainTex;
+		float2 uv_BumpMap;
+		float4 screenPos;
+	};
+
+	uniform fixed4 _Color;
+	uniform sampler2D _MainTex;
+	uniform sampler2D _BumpMap;
+	uniform float _ReflAmount;
+	uniform float _ReflDistort;
+	uniform sampler2D _ReflectionTex;
+
+	void surf(Input IN, inout SurfaceOutput o) {
+
+		fixed4 tex = tex2D(_MainTex, IN.uv_MainTex);
+		fixed3 nor = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));
+
+		fixed2 screenUV = (IN.screenPos.xy) / (IN.screenPos.w);
+		screenUV.xy += nor *_ReflDistort;
+
+		fixed4 refl = tex2D(_ReflectionTex, screenUV);
+
+		o.Albedo = tex.rgb *_Color.rgb;
+		o.Normal = nor.rgb;
+		o.Emission = refl *_ReflAmount *tex.a;
+		o.Specular = _Color.a;
+		o.Gloss = tex.a;
+		o.Alpha = tex.a;
+
+	}
+	ENDCG
+	}
+
+		FallBack "Diffuse"
+}
+
