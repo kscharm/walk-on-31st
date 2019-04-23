7ceb5143beb92b1c78c1b52eae7cf3390f9e567a
Ryan <jpower30@gatech.edu>
2019-04-23 15:58:12 -0400

    Running footstep sounds

--- nothing_BASIC/Assets/Scripts/Footsteps.cs
+++ nothing_BASIC/Assets/Scripts/Footsteps.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     private Rigidbody rbody;
+    private AudioSource footsteps;
+    public AudioClip singleFootstep;
+    public AudioClip singleFootstepRunning;
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody>();
+        footsteps = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update () {
-        if (rbody.velocity.magnitude > 1f && !GetComponent<AudioSource>().isPlaying) {
-            GetComponent<AudioSource>().Play();
+        if (rbody.velocity.magnitude > 2.5f && !footsteps.isPlaying) {
+            footsteps.clip = singleFootstepRunning;
+            footsteps.Play();
+        } else if (rbody.velocity.magnitude > 0 && !footsteps.isPlaying) {
+            footsteps.clip = singleFootstep;
+            footsteps.Play();
         }
+
     }
 }
