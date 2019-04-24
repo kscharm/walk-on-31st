58aac97d215614a9ef44238617e55743c0fb32b8
shudock3 <Sammi Hudock>
2019-04-24 15:27:18 -0400

    global class

---
+++ nothing_BASIC/Assets/Scripts/Global.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Global : MonoBehaviour
+{
+    public static float time = 120f;
+
+    public static string timerText;
+
+    public void Start()
+    {
+    }
+}
--- nothing_BASIC/Assets/Scripts/LevelController.cs
+++ nothing_BASIC/Assets/Scripts/LevelController.cs
@@ -5,29 +5,32 @@ using UnityEngine.SceneManagement;
 public class LevelController : MonoBehaviour
 {
     public Text timer;
-    public float timeLeft = 120f;
+   public float timeLeft = 120f;
     // Start is called before the first frame update
     void Start()
     {
+        timer.text = Global.timerText;
         SetTimerText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeLeft >= 0)
+        if (Global.time >= 0)
         {
-            timeLeft -= Time.deltaTime;
+            Global.time -= Time.deltaTime;
         }
         else
         {
             SceneManager.LoadScene("EndGame", LoadSceneMode.Single);
         }
+        timer.text = Global.timerText;
         SetTimerText();
     }
 
     void SetTimerText()
     {
-        timer.text = "Time remaining: " + Mathf.Round(timeLeft * 10f) / 10f;
+        timer.text = "Time remaining: " + Mathf.Round(Global.time * 10f) / 10f;
+        Global.timerText = timer.text;
     }
 }
