4a46fe807284ad7f6cf71527627a42ad61319a98
Ryan <jpower30@gatech.edu>
2019-04-25 13:31:09 -0400

    Global vars for all sauces and fish

--- nothing_BASIC/Assets/Scripts/Global.cs
+++ nothing_BASIC/Assets/Scripts/Global.cs
@@ -11,6 +11,15 @@ public class Global : MonoBehaviour
     public static bool goblin1Done;
     public static bool golbin2Done;
     public static bool hasGreenFish;
+    public static bool hasBlueFish;
+    public static bool hasYellowFish;
+    public static bool hasRedFish;
+    public static bool hasPurpleFish;
+    public static bool hasRedSauce;
+    public static bool hasBlueSauce;
+    public static bool hasGreenSauce;
+    public static bool hasPinkSauce;
+    public static bool hasOrangeSauce;
 
 
     public void Start()
--- nothing_BASIC/Assets/Scripts/SauceFishTracker.cs
+++ nothing_BASIC/Assets/Scripts/SauceFishTracker.cs
@@ -33,7 +33,28 @@ public class SauceFishTracker : MonoBehaviour
 
         if (Global.hasGreenFish) {
             greenFishCondition.satisfied = true;
+        } else if (Global.hasYellowFish) {
+            yellowFishCondition.satisfied = true;
+        } else if (Global.hasRedFish) {
+            redFishCondition.satisfied = true;
+        } else if (Global.hasPurpleFish) {
+            purpleFishCondition.satisfied = true;
+        } else if (Global.hasBlueFish) {
+            blueFishCondition.satisfied = true;
         }
+
+        if (Global.hasRedSauce) {
+            redSauceCondition.satisfied = true;
+        } else if (Global.hasBlueSauce) {
+            blueSauceCondition.satisfied = true;
+        } else if (Global.hasGreenSauce) {
+            greenSauceCondition.satisfied = true;
+        } else if (Global.hasPinkSauce) {
+            pinkSauceCondition.satisfied = true;
+        } else if (Global.hasOrangeSauce) {
+            orangeSauceCondition.satisfied = true;
+        }
+
     }
 
     // Update is called once per frame
@@ -41,7 +62,28 @@ public class SauceFishTracker : MonoBehaviour
     {   
         if (greenFishCondition.satisfied) {
             Global.hasGreenFish = true;
+        } else if (yellowFishCondition.satisfied) {
+            Global.hasYellowFish = true;
+        } else if (redFishCondition.satisfied) {
+            Global.hasRedFish = true;
+        } else if (purpleFishCondition.satisfied) {
+            Global.hasPurpleFish = true;
+        } else if (blueFishCondition.satisfied) {
+            Global.hasBlueFish = true;
         }
+
+        if (redSauceCondition.satisfied) {
+            Global.hasRedSauce = true;
+        } else if (blueSauceCondition.satisfied) {
+            Global.hasBlueSauce = true;
+        } else if (greenSauceCondition.satisfied) {
+            Global.hasGreenSauce = true;
+        } else if (pinkSauceCondition.satisfied) {
+            Global.hasPinkSauce = true;
+        } else if (orangeSauceCondition.satisfied) {
+            Global.hasOrangeSauce = true;
+        }
+
     }
 
     public bool isCorrectCombination() {
