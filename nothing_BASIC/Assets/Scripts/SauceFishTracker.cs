9e5690e23816bf1d0b258fe2b4088531088852d2
Gouri, Nandan J <ngouri3@gatech.edu>
2019-04-24 23:56:18 -0400

    fixed delay on goblins. fixed sauce comboes

--- nothing_BASIC/Assets/Scripts/SauceFishTracker.cs
+++ nothing_BASIC/Assets/Scripts/SauceFishTracker.cs
@@ -40,12 +40,12 @@ public class SauceFishTracker : MonoBehaviour
     }
 
     public bool isCorrectCombination() {
-        //Red Sauce + Yellow Fish
-        return redSauceCondition.satisfied && yellowFishCondition.satisfied;
+        //green Sauce + Yellow Fish
+        return greenSauceCondition.satisfied && yellowFishCondition.satisfied;
     }
     public bool isCorrectCombination2() {
-        //Orange Sauce + Purple Fish
-        return orangeSauceCondition.satisfied && purpleFishCondition.satisfied;
+        //Orange Sauce + Green Fish
+        return orangeSauceCondition.satisfied && greenFishCondition.satisfied;
     }
 
 }
