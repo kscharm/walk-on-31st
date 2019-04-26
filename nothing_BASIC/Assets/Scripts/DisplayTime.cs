1f3a69d873b4539c69d181500d2e15a7c7555479
shudock3 <Sammi Hudock>
2019-04-26 00:24:28 -0400

    timer at the end

--- nothing_BASIC/Assets/Scripts/DisplayTime.cs
+++ nothing_BASIC/Assets/Scripts/DisplayTime.cs
@@ -7,16 +7,34 @@ using UnityEngine.SceneManagement;
 public class DisplayTime : MonoBehaviour
 {
     public Text time;
+    public Text minute;
+    public Text seconds;
+    private float remaining;
+    private float minutes;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        remaining = Global.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time.text = "Your time: " + ((9.0f*60f) - Global.time) + " seconds";
+        remaining = (540f - Mathf.Round(Global.time * 10f) / 10f);
+        if (remaining > 60f)
+        {
+            //remaining += 2f;
+            minutes = remaining / 60;
+            remaining = remaining%60;
+            time.text = "Your time: ";
+            minute.text = minutes + " minutes and ";
+            seconds.text = +remaining + " seconds";
+        }
+        else
+        {
+            time.text = "Your time: " + (540f - Mathf.Round(Global.time * 10f) / 10f);
+            minute.text = " seconds";
+        }
     }
 }
