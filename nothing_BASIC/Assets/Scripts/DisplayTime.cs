b4934dc29caba81065bf625a98317f8768944723
shudock3 <Sammi Hudock>
2019-04-25 23:06:29 -0400

    UI

---
+++ nothing_BASIC/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DisplayTime : MonoBehaviour
+{
+    public Text time;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time.text = "Your time: " + ((9.0f*60f) - Global.time) + " seconds";
+    }
+}
